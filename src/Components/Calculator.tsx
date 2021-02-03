93643db9724b45161590c5d6418952fbe35816fd
Noémi Puskás <noemi.puskas@evocrm.hu>
2021-02-03 10:43:09 +0100

    Calculator: basic calculations added

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 let Calculator = () => {
-return(
-  <form className = 'calculator'>
-    <h1>Anyagköltség</h1>
-    <input type="text" className = "expense-material" inputMode="numeric"/>
-    <h1>Munkadíj</h1>
-    <input type="text" className = "expense-work" inputMode="numeric"/>
-    <div className = "output">
-      <h1>Elérhető támogatás:</h1>
-      <h1 className = "amount">{}</h1>
-    </div>
-  </form>
-)
+  const [material, setMaterial] = useState(0);
+  const [work, setWork] = useState(0);
+  const [output, setOutput] = useState(0);
+
+  let handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let name = event.target.name;
+    
+    if (name === "material"){
+      setMaterial(prevState => Number(event.target.value));
+    } else if (name === "work"){
+      setWork(prevState => Number(event.target.value));
+    }
+
+    setOutput(prevState => {
+      let sumExpense = (material + work)/4;
+      let smaller: number = 0;
+      if (material <= work){
+        smaller = material;
+      } else if (work < material){
+        smaller = work;
+      }
+
+      if(smaller < sumExpense){
+        return smaller * 2;
+      } else {
+        return  sumExpense / 2;
+      }
+    })
+  }
+
+
+  return (
+    <form className='calculator'>
+      <h1>Anyagköltség</h1>
+      <input type="text" className="expense-material" name="material" onChange={handleChange} value = {material} />
+      <h1>Munkadíj</h1>
+      <input type="text" className="expense-work" name="work" onChange={handleChange} value = {work}/>
+      <div className="output">
+        <h1>Elérhető támogatás:</h1>
+        <h1 className="amount">{output}</h1>
+      </div>
+    </form>
+  )
 }
 
 export default Calculator;
